d205c8c7b6fff3b7cbd65e17d5255293719b3c1a
danilocick <dhernandez@elpuig.xeill.net>
2021-04-13 17:07:22 +0200

    a

--- src/main/java/com/example/MP06UF6Spring/GreetingController.java
+++ src/main/java/com/example/MP06UF6Spring/GreetingController.java
@@ -2,17 +2,15 @@ package com.example.MP06UF6Spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //
 @RequestMapping(GreetingController.GREETING)
 public class GreetingController {
     public static final String GREETING = "/greeting";
 
-    @GetMapping
+    //para busquedas de POST: @PostMapping
+    @GetMapping //busquedas de GET
     public String greeting(@RequestParam(value = "name",required=false , defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
